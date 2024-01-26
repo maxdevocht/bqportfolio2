817a7b3ee83c849e2b6c47e30c0c578a92b29971
Max de Vocht <maxdevocht@hotmail.com>
2024-01-26 11:54:43 +0100

    Update SplideJS
    Adjusted the JS so that i can have multiple carousels

--- slider.js
+++ slider.js
@@ -1,25 +1,32 @@
-var splide = new Splide( '.splide', {
-    perPage: 1,
-    perMove: 1,
-    gap    : '1rem',
-    padding: '2rem',
-    type: 'loop',
-    drag: 'free',
-    snap: true,
-    autoplay: false,
-    breakpoints: {
-      640: {
-        perPage: 2,
-        gap    : '.7rem',
-        padding: '1.5rem',
-      },
-      480: {
+// Select all elements with the class 'splide' and create Splide instances for each.
+document.querySelectorAll('.splide').forEach(function (element) {
+    // Options for each Splide instance.
+    var options = {
         perPage: 1,
-        gap    : '.7rem',
-        padding: '1.5rem',
-      },
-    },
-} );
-  
-splide.mount();
+        perMove: 1,
+        gap: '1rem',
+        padding: '2rem',
+        type: 'loop',
+        drag: 'free',
+        snap: true,
+        autoplay: false,
+        breakpoints: {
+            640: {
+                perPage: 2,
+                gap: '.7rem',
+                padding: '1.5rem',
+            },
+            480: {
+                perPage: 1,
+                gap: '.7rem',
+                padding: '1.5rem',
+            },
+        },
+    };
+
+    // Create a new Splide instance for each element with the specified options.
+    new Splide(element, options).mount();
+});
+
+
   
